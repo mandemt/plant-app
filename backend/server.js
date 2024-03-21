fdfa50591691c6f30f22850e120d8c21f75bb74d
mandemt <70643363+mandemt@users.noreply.github.com>
2024-03-21 17:55:04 +0100

    plant watch part II

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,58 @@ app.get('', (req, res) => {
 })
 app.post('/planten/:id', (req, res) => {
     let id = req.body[0].id
-    console.log(id)
-    const sql = "SELECT * FROM plants WHERE id=?";
-    db.query(sql, id,(err, data) => {
-console.log(data)
-        return res.json(data)
+    // const query = "SELECT * FROM plants  AS planten JOIN profile AS p ON p.plant_id = planten.plant_id WHERE planten.plant_id=?"
+    // db.query(query, id, (err, data) => {
+    //     result.push(data[0].watched)
+    // return res.json(data)
+    // })
+
+
+const registerWatch = [{getData: "SELECT * FROM plants  AS planten JOIN profile AS p ON p.plant_id = planten.plant_id WHERE planten.plant_id=?"},{postData: "UPDATE profile SET watched=? WHERE plant_id = ?"}]
+
+      db.query(registerWatch[0].getData, id, (err, data) => {
+    let result = data
+    postData(Number(result[0].watched))
+
+    return res.json(data)
+   function postData(result){
+        db.query(registerWatch[1].postData, [(result + 1), id],(err, data) => {
+        })
+    }
     })
+    
+    
+
+
+// db.query(registerWatch, result, id, (err, data) => {
+// })
+    // let q_res = queries.map((q) => {
+    //     return db.query(q, id, (err, data) => {
+    //         console.log(data)
+    //         return data;
+    //     });
+    // });
+
+
+//   console.log(JSON.stringify(result))
+    // return {
+    //     profiles: q_res[0],
+    //     planten: q_res[1]
+    // }
+
+    // console.log(res.json(q_res));
+
+    //return res.json(q_res);
+
+
+//  const sql2 = "SELECT * FROM plants WHERE id=?"
+// db.query(sql, id, (err, data) => {
+
+// useData(data)
+// })
+// function useData(jo){
+//  return res.json(jo)
+// }
 })
 app.get('/planten', (req, res) => {
     const sql = "SELECT * FROM plants";
@@ -58,7 +104,7 @@ app.get('/anders', (req, res) => {
 
 app.post('/anders', (req, res) => {
 let insertData = req.body[0]
-    const sql = "INSERT INTO plants (id, plant_name, plant_family, plant_hoofdgroep, plant_bloemkleur, invasief) VALUES (NULL, ?,?,?,?,?)"
+    const sql = "INSERT INTO plants (plant_id, plant_name, plant_family, plant_hoofdgroep, plant_bloemkleur, invasief) VALUES (NULL, ?,?,?,?,?)"
 
     db.query(sql, [insertData.name,insertData.family,insertData.group,insertData.color,insertData.invasive], (err, data) => {
         console.log(data)
